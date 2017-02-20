6180dd3a8ee3878715e4675f056a5bd4930b447b
Steve Tarzia <steve@stevetarzia.com>
2017-02-20 13:26:01 -0600

    save cookies to prevent Google from thinking we are a bot

--- scraper.py
+++ scraper.py
@@ -108,8 +108,10 @@ def load_CVs():
                 all_CVs[slug] = f.read()
     return all_CVs
 
+http_session = requests.Session()
+
 def get_tree(url):
-    r = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"})
+    r = http_session.get(url, headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"})
     if r.status_code != 200:
         print "WARNING got %d status code for %s" % (r.status_code, url)
     return lxml.html.fromstring(r.text)
