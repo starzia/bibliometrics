ab53deafc7e730ddc326f2c2d9dd47f1505dcb60
Steve Tarzia <steve@stevetarzia.com>
2017-03-01 13:24:22 -0600

    Scrape Harvard pubs

--- school/harvard.py
+++ school/harvard.py
@@ -1,5 +1,19 @@
 from professor_scraper import scrape_professors
-from web_util import Selector, HrefSelector, HrefListSelector
+from web_util import Selector, HrefSelector, HrefListSelector, css_select, strip_whitespace
+
+
+def get_papers(url, tree):
+    # find the bulleted list for publications
+    for heading in css_select(tree, '.tab-content h3'):
+        if 'Articles' in heading.text:
+            # look for the first <OL> under the publications header
+            next = heading
+            while next is not None:
+                if next.name == 'ol':
+                    return url, [strip_whitespace(li.text.replace('View Details', '').replace('Citation:', ''))
+                                 for li in css_select(next, 'div.citation')]
+                next = next.next_element
+    return None, None
 
 
 def scrape_harvard():
@@ -10,7 +24,8 @@ def scrape_harvard():
                              extracts_name=Selector('h1.author'),
                              extracts_cv_url=HrefSelector('div.faculty-navigation div.links a', 'Curriculum Vitae'),
                              extracts_personal_url=HrefSelector('div.faculty-navigation div.links a',
-                                                                'Personal Website', 'Home Page'))
+                                                                'Personal Website', 'Home Page'),
+                             extracts_papers=get_papers)
 
 if __name__ == '__main__':
     profs = scrape_harvard()
