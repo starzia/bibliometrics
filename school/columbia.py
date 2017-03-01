6c7691e36f451ae3eabf5b0a04a9cfa386a67ca0
Steve Tarzia <steve@stevetarzia.com>
2017-03-01 12:09:43 -0600

    Scrape Columbia pub lists

--- school/columbia.py
+++ school/columbia.py
@@ -1,5 +1,21 @@
 from professor_scraper import scrape_professors
-from web_util import Selector, HrefSelector, HrefListSelector
+from web_util import Selector, HrefSelector, HrefListSelector, strip_whitespace, css_select
+
+
+def get_papers(url, tree):
+    papers = []
+    # find the list of divs for journal publications
+    for heading in css_select(tree, 'h3'):
+        if 'Journal articles' in heading.text:
+            # keep collecting the publication divs until we get to the next <H3>
+            next = heading
+            while next is not None:
+                if next.name == 'p':
+                    papers.append(strip_whitespace(next.text))
+                elif next.name == 'div' and 'Awards and Honors' in next.text:
+                    break
+                next = next.next_element
+    return url+'#research', papers
 
 
 def scrape_columbia():
@@ -11,7 +27,8 @@ def scrape_columbia():
                              # for CV and personal website, see http://www8.gsb.columbia.edu/cbs-directory/detail/ea1
                              extracts_cv_url=HrefSelector('div#contact_info a', 'Curriculum Vitae'),
                              extracts_personal_url=HrefSelector('div#contact_info a', 'Personal Website'),
-                             extracts_gscholar_url=None)
+                             extracts_gscholar_url=None,
+                             extracts_papers=get_papers)
 
 if __name__ == '__main__':
     profs = scrape_columbia()
