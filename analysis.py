219e1dd0073aac07daeddd703441c6411ca8acc4
Steve Tarzia <steve@stevetarzia.com>
2017-03-21 12:58:50 -0500

    Calculate prestigious publication rate and citation aging trend

--- analysis.py
+++ analysis.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 from professor import Professor
-from typing import List
+from typing import List, Dict, AnyStr
 from professor_scraper import load_paper_list
 from google_scholar import Paper, STARTING_YEAR, get_year
 from google_sheets import GoogleSheets
@@ -95,6 +95,10 @@ HIGH_COLLISION_TOP_JOURNALS = [
 pp = pprint.PrettyPrinter(indent=4)
 non_letter_remover = str.maketrans('', '', string.punctuation + string.digits)
 
+# global plotting parameters
+plt.rcParams["font.serif"] = "Times New Roman"
+plt.rcParams["font.family"] = "serif"
+
 
 def abbreviate(journal_name):
     output = []
@@ -131,19 +135,30 @@ try:
         ignored_scholar_citation_ids = set([l.replace('\n', '') for l in f.readlines()])
 except FileNotFoundError:
     print("WARNING: did not find papers_to_ignore.txt")
+paper_cache = {}
 
 
 def load_papers(prof):
-    global paper_folders, starting_year
-    papers = []
-    for f in paper_folders:
-        papers.extend([p for p in [Paper.from_string(s) for s in load_paper_list(f, prof)]
-                       if p.year > starting_year and p.id not in ignored_scholar_citation_ids])
-    return papers
+    global paper_folders, starting_year, paper_cache
+    if prof not in paper_cache:
+        papers = []
+        for f in paper_folders:
+            papers.extend([p for p in [Paper.from_string(s) for s in load_paper_list(f, prof)]
+                           if p.year > starting_year and p.id not in ignored_scholar_citation_ids])
+        paper_cache[prof] = papers
+    return paper_cache[prof]
 
 
 def count_citations(prof):
     return sum(int(paper.scholar_citations) for paper in load_papers(prof))
+
+
+def citation_aging_report(profs) -> List[List[int]]:
+    """:return: the number of citations for professors of "age" 0 through 10 years.  Anyone older than 10 is considered
+     10 because we ignore papers older than ten years anyway."""
+    return [[count_citations(p) for p in profs if p.graduation_year and
+             (2017 - int(p.graduation_year) == i if i < 10 else 2017 - int(p.graduation_year) >= i)]
+            for i in range(0, 11)]
 
 
 def load_papers_including_html(prof):
@@ -158,7 +173,7 @@ def load_papers_including_html(prof):
     return candidates
 
 
-def count_papers_in_top_journals(professors: List[Professor]):
+def papers_in_top_journals(professors: List[Professor]) -> Dict[Professor, AnyStr]:
     """:return dict mapping from professor to a list of titles"""
     top_papers = {}
     for p in professors:
@@ -179,16 +194,15 @@ def top_journal_pubs_for(school, professors: List[Professor]):
     return len([citation for (journal, citation) in candidates if is_a_top_journal(journal)])
 
 
-def top_journal_pubs_for_profs_at_school(professors: List[Professor]):
-    top_papers = count_papers_in_top_journals(professors)
-    return {school: [len(top_papers[p]) for p in professors if p.school == school] for school in SCHOOLS}
+def top_journal_pubs_for_profs_at_school(top_papers_for_each_prof: Dict[Professor, List[AnyStr]]) -> Dict[AnyStr, int]:
+    """ :return: a dict mapping each school to a list of publication counts, one for each prof at that school."""
+    return {school: [len(papers) for prof, papers in top_papers_for_each_prof.items() if prof.school == school]
+            for school in SCHOOLS}
 
 
-def print_top_journal_results(professors):
-    top_papers = count_papers_in_top_journals(professors)
-
-    for p, papers in top_papers.items():
-        print(p.slug())
+def print_top_journal_results(top_papers_for_each_prof: Dict[Professor, List[AnyStr]]):
+    for prof, papers in top_papers_for_each_prof.items():
+        print(prof.slug())
         for paper in papers:
             print('\t' + paper)
 
@@ -199,8 +213,8 @@ def pubs_for_school_in_journal(professors, journal_name):
             for school in SCHOOLS}
 
 
-def norm_str(string):
-    return strip_whitespace(string.lower().translate(non_letter_remover))
+def norm_str(my_string):
+    return strip_whitespace(my_string.lower().translate(non_letter_remover))
 
 
 def strings_are_similar(string1, string2):
@@ -249,10 +263,6 @@ def citations_for_profs_in_school(professors: List[Professor]):
     return school_citations
 
 
-def school_fcn(school_dict, fcn):
-    return {school: fcn(school_dict[school]) for school in SCHOOLS}
-
-
 def h_index_by_school(professors: List[Professor]):
     school_h_index= {}
     for school in SCHOOLS:
@@ -289,6 +299,11 @@ def normalize(school_dict, professors):
     return normalized
 
 
+def normalize_to_age(top_papers_for_each_prof: Dict[Professor, List[AnyStr]]) -> Dict[Professor, float]:
+    return {prof: len(top_papers_for_each_prof[prof])/(2018 - int(prof.graduation_year))
+            for prof in [p for p in top_papers_for_each_prof if p.graduation_year]}
+
+
 def plot(pdf_pages, title, dict):
     print('\n%s:' % title)
     data = sort_and_print_dict(dict)
@@ -308,14 +323,35 @@ def plot(pdf_pages, title, dict):
     plt.close()
 
 
+def plot_aging(aging):
+    fig, ax1 = plt.subplots()
+    ax1.bar([age for age, cites in enumerate(aging)],
+            [len(cites) for age, cites in enumerate(aging)],
+            label="sample size", color=[0.8,0.8,0.8])
+    plt.xticks([age for age, cites in enumerate(aging)],
+               [age if age < 10 else '>=10' for age, cites in enumerate(aging)])
+    ax1.set_xlabel("Number of years since faculty's graduation")
+    ax2 = plt.gca().twinx()
+    ax2.plot([age for age, cites in enumerate(aging)],
+             [statistics.median(cites) for age, cites in enumerate(aging)],
+             label="median citations")
+    ax2.plot([age for age, cites in enumerate(aging)],
+             [statistics.mean(cites) for age, cites in enumerate(aging)], '--',
+             label="mean citations")
+    ax2.legend(loc=1)
+    ax1.legend(loc=2)
+    ax2.set_ylabel("Citations per faculty")
+    ax1.set_ylabel("Number of faculty")
+    ax1.set_ylim([0, 700])
+    ax2.set_ylim([0, 1700])
+    plt.savefig("aging.pdf")
+    plt.close()
+
 def all_analyses():
     gs = GoogleSheets()
     profs = gs.read_profs()
     # remove hidden profs
     profs = [p for p in profs if not p.hidden]
-
-    # # just consider profs who finished their PhD within the past ten years
-    # profs = [p for p in profs if p.graduation_year and p.graduation_year >= starting_year]
 
     global paper_folders, starting_year
     # # just consider profs with a google scholar profile
@@ -324,24 +360,42 @@ def all_analyses():
     # print('Only considering the %d professors with Google Scholar profiles.' % len(profs))
 
     print('Looking exclusively at papers published in %s and later.' % starting_year)
+    aging = citation_aging_report(profs)
+    plot_aging(aging)
 
-    pp = PdfPages('plots.pdf')
-    plot(pp, 'Professor count', {school:len([p for p in profs if p.school==school]) for school in SCHOOLS})
+    run_analyses(profs, "plots_all.pdf")
+    # just consider profs who finished their PhD within the past ten years
+    run_analyses([p for p in profs if p.graduation_year and p.graduation_year >= starting_year], "plots_early.pdf")
+
+
+def run_analyses(profs, pdf_output_filename):
+    pp = PdfPages(pdf_output_filename)
+    plot(pp, 'Faculty count', {school:len([p for p in profs if p.school==school]) for school in SCHOOLS})
 
     citations = citations_for_profs_in_school(profs)
-    plot(pp, 'Citations', school_fcn(citations, sum))
-    plot(pp, 'Mean citations/prof', normalize(school_fcn(citations, sum), profs))
-    plot(pp, 'Median citations/prof', school_fcn(citations, statistics.median))
+    citations_per_school = {school: sum(cites) for school, cites in citations.items()}
+    plot(pp, 'Citations', citations_per_school)
+    plot(pp, 'Mean citations per faculty', normalize(citations_per_school, profs))
+    plot(pp, 'Median citations per faculty', {school: statistics.median(cites) for school, cites in citations.items()})
 
     plot(pp, 'School h-index', h_index_by_school(profs))
 
     j_stats = {school: top_journal_pubs_for(school, profs) for school in SCHOOLS}
-    plot(pp, 'Publications in "top" journals', j_stats)
-    plot(pp, 'Mean top-pubs/prof', normalize(j_stats, profs))
-    plot(pp, 'Median top-pubs/prof',
-                      school_fcn(top_journal_pubs_for_profs_at_school(profs), statistics.median))
+    plot(pp, 'Prestigious publication count', j_stats)
+    plot(pp, 'Mean prestigious publications per faculty', normalize(j_stats, profs))
+    top_papers = papers_in_top_journals(profs)
+    plot(pp, 'Median prestigious publications per faculty',
+         {school: statistics.median(cites)
+          for school, cites in top_journal_pubs_for_profs_at_school(top_papers).items()})
+    prof_ppub_rate = normalize_to_age(top_papers);
+    plot(pp, 'Mean faculty prestigious publication rate',
+         {school: statistics.mean([rate for prof, rate in prof_ppub_rate.items() if prof.school == school])
+          for school in SCHOOLS})
+    plot(pp, 'Median faculty prestigious publication rate',
+         {school: statistics.median([rate for prof, rate in prof_ppub_rate.items() if prof.school == school])
+          for school in SCHOOLS})
     for j in TOP_JOURNALS:
-        plot(pp, j, pubs_for_school_in_journal(profs, j))
+        plot(pp, j, pubs_for_school_in_journal(top_papers, j))
     pp.close()
 
 
