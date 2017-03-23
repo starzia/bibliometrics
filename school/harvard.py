1d003e9cb2378265b734a9195c2881df1613616a
Steve Tarzia <steve@stevetarzia.com>
2017-03-23 11:07:03 -0500

    Fixed Harvard’s publication list scraper

--- school/harvard.py
+++ school/harvard.py
@@ -1,17 +1,21 @@
 from professor_scraper import scrape_professors
-from web_util import Selector, HrefSelector, HrefListSelector, css_select, strip_whitespace
+from web_util import Selector, HrefSelector, HrefListSelector, css_select, strip_whitespace, wait, get_tree
 
 
 def get_papers(url, tree):
+    # add "&facInfo=pub" to the faculty url to get the url for the publications tab
+    pub_list_url = url + "&facInfo=pub"
+    wait()
+    pub_tree = get_tree(pub_list_url)
     # find the bulleted list for publications
-    for heading in css_select(tree, '.tab-content h3'):
+    for heading in css_select(pub_tree, '.tab-content h3'):
         if 'Articles' in heading.text:
             # look for the first <OL> under the publications header
             next = heading
             while next is not None:
                 if next.name == 'ol':
-                    return url, [strip_whitespace(li.text.replace('View Details', '').replace('Citation:', ''))
-                                 for li in css_select(next, 'div.citation')]
+                    return pub_list_url, [strip_whitespace(li.text.replace('View Details', '').replace('Citation:', ''))
+                                          for li in css_select(next, 'div.citation')]
                 next = next.next_element
     return None, None
 
