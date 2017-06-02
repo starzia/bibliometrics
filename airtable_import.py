8fdc9b7c732233e4a65f7ae2d78b219fbdc8319b
Steve Tarzia <steve@stevetarzia.com>
2017-06-02 14:30:48 -0500

    handle importing profs with missing data

--- airtable_import.py
+++ airtable_import.py
@@ -16,29 +16,45 @@ def airtable_import():
     airtable_API_key = getpass.getpass("Enter your AirTable API key: ")
 
     # get iterate through all customers in airtable
-    response = get_json(
-        "https://api.airtable.com/v0/apptzGHIQmdYv9F1i/Customers?maxRecords=999&view=Grid%20view&api_key="
-        + airtable_API_key)
-    for customer in response["records"]:
+    customers = []
+    base_url = "https://api.airtable.com/v0/apptzGHIQmdYv9F1i/Customers?view=Grid%%20view&api_key=%s"
+    offset = None
+    while True:
+        if offset:
+            base_url += "&offset=" + offset
+        response = get_json(base_url % airtable_API_key)
+        customers.extend(response["records"])
+        if "offset" not in response:
+            break
+        offset = response["offset"]
+
+    for customer in customers:
         if "Professor" in customer["fields"]["Role"]:
             print("found professor: " + customer["fields"]["Full Name"])
             netId = customer["fields"]["NetId"]
-            prof = [p for p in profs if netId in p.faculty_directory_url][0]
+            matching_profs = [p for p in profs if netId in p.faculty_directory_url]
+            if len(matching_profs) == 0:
+                print("WARNING: not present in Google Sheets")
+                continue
+            prof = matching_profs[0]
             # update the professor's airtable record
             update_body = {
                 "fields": {
-                    "Directory URL": prof.faculty_directory_url,
-                    "Photo": [{
-                        "url": get_photo_url(prof),
-                        "filename": netId + "_headshot.jpg"
-                    }],
-                    "CV": [{
-                        "url": prof.cv_url,
-                    }]
+                    "Directory URL": prof.faculty_directory_url
                 }
             }
+            photo_url = get_photo_url(prof)
+            if photo_url:
+                update_body["fields"]["Photo"] = [{
+                    "url": get_photo_url(prof),
+                    "filename": netId + "_headshot.jpg"
+                }]
             if prof.google_scholar_url:
                 update_body["fields"]["Scholar URL"] = prof.google_scholar_url
+            if prof.cv_url:
+                update_body["fields"]["CV"]: [{
+                    "url": prof.cv_url,
+                }]
             r = requests.patch("https://api.airtable.com/v0/apptzGHIQmdYv9F1i/Customers/" + customer["id"],
                                data=json.dumps(update_body),
                                headers={"Content-type": "application/json",
@@ -49,8 +65,10 @@ def airtable_import():
 
 
 def get_photo_url(prof):
-    img_tag = css_select(get_tree(prof.faculty_directory_url), "img.profileImg")[0]
-    return "https://www.kellogg.northwestern.edu" + img_tag.get("src")
+    img_tags = css_select(get_tree(prof.faculty_directory_url), "img.profileImg")
+    if len(img_tags) == 0:
+        return None
+    return "https://www.kellogg.northwestern.edu" + img_tags[0].get("src")
 
 
 if __name__ == '__main__':
